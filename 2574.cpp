386e0a7fa18d29962cf285ffe03f861346916ca9
Aniket Patil <109419194+Ronixa584@users.noreply.github.com>
2023-02-27 12:30:31 +0530

    Left and Right Sum Differences

---
+++ 2574.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> leftRigthDifference(vector<int>& nums) {
+    vector<int>ans;
+    int lsum=0,rsum=0;
+        for(int i=0;i<nums.size();i++) rsum+=nums[i];
+        for(int i=0;i<nums.size();i++){
+            rsum-=nums[i];
+            ans.push_back(abs(lsum-rsum));
+            lsum+=nums[i];
+        }
+        return ans;
+    }
+};
