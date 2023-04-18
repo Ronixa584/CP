36a92dac073fee4d51e3b569178d2cf00e3cf343
Aniket Patil <109419194+Ronixa584@users.noreply.github.com>
2023-04-18 08:54:57 +0530

    Kth Largest Element in an Array

---
+++ 215.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>pq(nums.begin(),nums.end());
+        while(k!=1){
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
