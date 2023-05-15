0ca87c13b4ed44380e94094882b5c970307b3e05
Aniket Patil <109419194+Ronixa584@users.noreply.github.com>
2023-05-15 16:54:55 +0530

    Matrix Diagonal Sum

---
+++ 1572.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n= mat.size();
+        long long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];
+            if(i!=n-i-1){
+                sum+=mat[i][n-i-1]; 
+            }
+        }
+        return sum;
+    }
+};
